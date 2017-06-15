29b0336ef952db474285a2df248401239dbf1db6
李静莹 <lijingying@lijingyingdeMacBook-Pro.local>
2017-06-15 17:31:16 +0800

    上传测试

---
+++ test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 李静莹 on 17/6/15.
+//  Copyright © 2017年 LiJingYing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 李静莹 on 17/6/15.
+//  Copyright © 2017年 LiJingYing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
